656e9dbf131fb10af81872193574f095e91fb6ee
L87R78 <l87r78@gmail.com>
2019-08-16 06:23:32 +0300

    Updates

---
+++ precache-manifest.d41a7c775fc2353df1fd8bdf653aef31.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ade835f76af0f423c79da7af65cd200e",
+    "url": "/PC-store/index.html"
+  },
+  {
+    "revision": "73b863c33db0a26852c0",
+    "url": "/PC-store/static/css/main.953bc835.chunk.css"
+  },
+  {
+    "revision": "c50c6c6bf672aa0d82fc",
+    "url": "/PC-store/static/js/2.ccf209ba.chunk.js"
+  },
+  {
+    "revision": "73b863c33db0a26852c0",
+    "url": "/PC-store/static/js/main.76ea4d59.chunk.js"
+  },
+  {
+    "revision": "7c31bb5327c8798a8d9b",
+    "url": "/PC-store/static/js/runtime~main.2a1206f3.js"
+  },
+  {
+    "revision": "4560c93f7da86906c430531e068616b3",
+    "url": "/PC-store/static/media/VR.4560c93f.PNG"
+  },
+  {
+    "revision": "501465f99be5dc0372691a4031fd7080",
+    "url": "/PC-store/static/media/background_laptops.501465f9.jpg"
+  },
+  {
+    "revision": "eb9b07f8504b71c849a965c299139cc4",
+    "url": "/PC-store/static/media/cart_background.eb9b07f8.jpg"
+  },
+  {
+    "revision": "23303e190649f38b05a2123be5057213",
+    "url": "/PC-store/static/media/center_image.23303e19.PNG"
+  },
+  {
+    "revision": "e5c3636c250c83bfd56f6398a6c337ae",
+    "url": "/PC-store/static/media/components_background.e5c3636c.jpg"
+  },
+  {
+    "revision": "895f2e9e15fc76ebe7113e829ef693e5",
+    "url": "/PC-store/static/media/configure_Your PC.895f2e9e.PNG"
+  },
+  {
+    "revision": "69047b5f3b33d6a14470d4a16bcc33a5",
+    "url": "/PC-store/static/media/contacts_image.69047b5f.PNG"
+  },
+  {
+    "revision": "8e75f25a794f202afce8c266a7e5bcff",
+    "url": "/PC-store/static/media/desktops.8e75f25a.PNG"
+  },
+  {
+    "revision": "098912fff1b27a68e9d60014636aa8c6",
+    "url": "/PC-store/static/media/displays.098912ff.PNG"
+  },
+  {
+    "revision": "c81bd0b749c7110bdc67f3bb154cbf03",
+    "url": "/PC-store/static/media/laptops.c81bd0b7.PNG"
+  },
+  {
+    "revision": "2aa3ea87192436633b6e0c9c58b244e7",
+    "url": "/PC-store/static/media/not_found_image.2aa3ea87.PNG"
+  }
+]);
