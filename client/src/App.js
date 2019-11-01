722f3f051561650eff2e6dcb65e6a6d7e4cde081
L87R78 <l87r78@gmail.com>
2019-11-01 10:34:05 +0200

    modified package.json

--- client/src/App.js
+++ client/src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            <h2>ahhaahhahahahahaha</h2>
           </p>
           <a
             className="App-link"
